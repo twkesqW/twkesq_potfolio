579e948a896932f769d51c0606592087c62a8c6b
twkesq <skuzemchenko50@gmail.com>
2023-06-03 17:10:43 +0300

    v1.3

--- src/components/heroimg1/HeroImg.js
+++ src/components/heroimg1/HeroImg.js
@@ -11,7 +11,7 @@ const HeroImg = () => {
     </div>
     <div className="content">
         <p className="animate__animated animate__fadeInDown  animate__delay-1s animate__slow">HI! I'm Freelancer.</p>
-        <h1><Typed strings={["Frontend Developer"]} typeSpeed={120}/></h1>
+        <h1>Frontend Develover</h1>
         <div>
           {/* <Link to="/myskills" className="btn">Skills</Link> */}
           <Link to="/contact" className="btn btn-light">Contact</Link>
